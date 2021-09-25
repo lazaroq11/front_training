99a441704cc46d4288ac9f720db5644fc7aa608c
Lázaro Queiroz <lazaroq11@hotmail.com>
2021-09-24 23:26:18 -0300

    feat carrossel
    Add carrossel na tela de perfis

---
+++ VitaoProfile/index.js
@@ -0,0 +1,17 @@
+const listaDePessoas = [
+  {
+    nome: "Vitor Manoel",
+    nick: "CremosinhoBA",
+    descricao:
+      "Vitor Manoel, desenvolvedor em desenvolvimento, atualmente fazendo pesquisa na UNEB-ACSO. Atualmente estudando tecnologias web. Não menos importante, mas ama bastante linguagens baixo nível e é joga em emuladores de super nintendo ao sentir nostalgia.",
+    descricaoModal:
+      "Vitor Manoel, desenvolvedor em desenvolvimento, atualmente fazendo pesquisa na UNEB-ACSO. Atualmente estudando tecnologias web. Não menos importante, mas ama bastante linguagens baixo nível e é joga em emuladores de super nintendo ao sentir nostalgia. Abertot a propostas de trabalho, estou habituado com html e css, conhecendo também o básico de programação web utilizando PHP. Costumo escrever algoritmos de baixo nível para realização de desafios e projetos de graduação utilizando C,C++ e Python.",
+    foto: "./images/profile.jpg",
+    atividadesFavoritas: [
+      "./images/activity.png",
+      "./images/activity1.png",
+      "./images/activity2.png",
+      "./images/activity3.png",
+    ],
+  },
+];
